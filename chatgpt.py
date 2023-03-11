f84517ad1fb49fa593f80fe12bf0098e56db7589
Michael Flynn <mrmichael.flynn@outlook.com>
2023-03-11 10:31:20 +0000

    update

--- chatgpt.py
+++ chatgpt.py
@@ -4,29 +4,28 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("prompt", help="The prompt to send to the OpenAI API")
-parser.add_argument("file_name", help="Name of the file to save Terraform code")
 args = parser.parse_args()
 
 api_endpoint = "https://api.openai.com/v1/completions"
 api_key = os.getenv("OPENAI_API_KEY")
 
+
 request_headers = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer " + api_key
+    "Authorization": f"Bearer {api_key}"
 }
 
 request_data = {
-    "model": "text-davinci-003",
-    "prompt": f"Write terraform code to {args.prompt}. Provide only code, no text",
+    "prompt": f"{args.prompt}",
     "max_tokens": 500,
-    "temperature": 0.5
+    "temperature": 0.5,
+    "engine": "davinci-codex"
 }
 
 response = requests.post(api_endpoint, headers=request_headers, json=request_data)
 
 if response.status_code == 200:
     response_text = response.json()["choices"][0]["text"]
-    with open(args.file_name, "w") as file:
-        file.write(response_text)
+    print(response_text)
 else:
     print(f"Request failed with status code: {str(response.status_code)}")
